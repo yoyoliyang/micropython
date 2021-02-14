e8befb10e0223a6c19fd8ba19f2c444b8e599494
yang <yoyoliyang@gmail.com>
2021-02-14 18:02:10 +0800

    删除uptime功能函数，该函数可能会导致socket错误，估计跟异步有关系

--- oray_ddns_def.py
+++ oray_ddns_def.py
@@ -28,17 +28,10 @@ def do_connect(ssid, pswd, wlan):
         while not wlan.isconnected():
             blink()
             pass
+
+    # wifi状态指示灯
+    Pin(2, Pin.OUT)
     print('network config:', wlan.ifconfig())
-
-
-# wifi检测，异步函数，用来检测wifi连接是否正常
-async def check_connect(ssid, pswd, wlan):
-    while True:
-        if not wlan.isconnected():
-            wlan.connect(ssid, pswd)
-            while not wlan.isconnected():
-                blink()
-                pass
 
 
 # ddns解析, ddns需要和web_server异步进行，故使用异步任务来调度
@@ -46,10 +39,10 @@ async def ddns():
     # user = "your username"
     # pswd = "your pswd"
     # 花生壳的ddns需要Basic认证，故header需要定义认证用户名和密码（base64转换后的）
-    headers = {'Authorization': 'Basic #############',
+    headers = {'Authorization': 'Basic #################',
                'User-Agent': 'curl/7.64.0'}
     host = "ddns.oray.com"
-    domain = "yourdomain.vicp.net"
+    domain = "xxxxxx.vicp.net"
     uri = "http://{}/ph/update?hostname={}".format(host, domain)
 
     # 每隔10分钟去更新一次域名解析
@@ -60,12 +53,13 @@ async def ddns():
             print(r.text)
             ddns_result = r.text
             await uasyncio.sleep(10 * 60)
-        except:
+        except OSError as e:
+            ddns_result = str(e)
             continue
 
 
 # web_server 端口为80 用来查看ddns状态
-async def web_server(poweron_time):
+async def web_server():
 
     s = socket.socket()
 
@@ -76,28 +70,21 @@ async def web_server(poweron_time):
 
     while True:
         cl, addr = s.accept()
-        uptime = time.time() - poweron_time
-        if (uptime // (60 * 60)) >= 1:
-            uptime = uptime // (60 * 60)
-            uptime = "{} hours".format(uptime)
-        else:
-            uptime = "{} seconds".format(uptime)
         try:
             data = cl.recv(1024)
-        except:
-            pass
+        except OSError:
+            cl.close()
+            continue
+        print('client connected from', addr, data)
         data = data.decode()
-        print('client connected from', addr)
-        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-        cl.send("ddns status: {}</br>".format(ddns_result))
-        cl.send("uptime: {}</br>".format(uptime))
-        cl.send("{}</br>".format(data[:40]))
+        response = ['HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n',
+                    "ddns status: {}".format(ddns_result)
+                    ]
+        cl.send("".join(response))
         cl.close()
 
 # ddns解析后的response,由ddns函数和web函数来使用
 ddns_result = ""
-# poweron time开机时的时间戳
-poweron_time = time.time()
 
 # WIFI
 SSID = 'SSID'
@@ -115,13 +102,9 @@ WLAN.ifconfig(TCPIP)
 # 开机后的网络连接
 do_connect(SSID, PSWD, WLAN)
 
-# wifi状态指示灯
-Pin(2, Pin.OUT)
-
 # 此处创建多个异步任务，分别处理ddns解析和webserver等任务
 event_loop = uasyncio.get_event_loop()
 event_loop.create_task(ddns())
-event_loop.create_task(web_server(poweron_time))
-event_loop.create_task(check_connect(SSID, PSWD, WLAN))
+event_loop.create_task(web_server())
 
 event_loop.run_forever()
